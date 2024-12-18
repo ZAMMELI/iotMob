1cdfd923b4b7e6f0c180738dd0948729112f0c5b
ZAMMELI <aymen.01.zammeli@gmail.com>
2024-12-18 23:52:05 +0100

    add signup

--- src/app/person-add/person-add.page.ts
+++ src/app/person-add/person-add.page.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { PersonAddService } from './person-add.service';
+import { Router } from '@angular/router';  // Import Router
 
 @Component({
   selector: 'app-person-add',
   templateUrl: './person-add.page.html',
   styleUrls: ['./person-add.page.scss'],
 })
-export class PersonAddPage implements OnInit {
+export class PersonAddPage {
+  userData = {
+    username: '',
+    email: '',
+    password: '',
+  };
 
-  constructor() { }
+  constructor(
+    private personAddService: PersonAddService,
+    private router: Router  // Inject Router into the constructor
+  ) {}
 
-  ngOnInit() {
+  onSignup() {
+    this.personAddService.registerUser(this.userData).subscribe(
+      (response) => {
+        console.log('Signup successful:', response);
+        alert('Signup successful!');
+        this.router.navigate(['/home']);// Navigate to home page after successful signup
+        
+        // Clear the form fields after successful signup
+        this.userData = {
+          username: '',
+          email: '',
+          password: '',
+        };
+      },
+      (error) => {
+        console.error('Error during signup:', error);
+        alert('Signup failed. Please try again.');
+      }
+    );
   }
-
 }
